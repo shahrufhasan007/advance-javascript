41d63c9f0a56ce4d734d3e9b7fbf097fe4eed97b
shahrufhasan007 <shahrufhassan@gmail.com>
2020-08-10 21:57:16 +0600

    null-vs-undefined

---
+++ Null-vs-Undefined.js
@@ -0,0 +1,19 @@
+// let name;
+
+// function add(num1, num2) {
+//     console.log(num1 + num2)
+//     return
+// }
+
+// function add(num1, num2) {
+//     console.log(num2);
+// }
+
+// const person = { name: "shahruf", phone: 269854564 };
+// console.log(person.age);
+
+// let fun = undefined;
+// console.log(fun)
+
+// let ages = [19, 21, 23, 29];
+// console.log(ages[11]);
